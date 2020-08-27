da3324e8899bd373c09b581207ec10fc614175af
Aislan Aprovar <aislan_85@yahoo.com.br>
2020-08-27 09:08:42 -0300

    salvar/editar e consulta se cliente ja cadastrado

--- assets/class/usuarios.class.php
+++ assets/class/usuarios.class.php
@@ -1,5 +1,6 @@
 <?php
 class Usuarios{
+    private $id;
     private $nome;
     private $senha;
     private $email;
@@ -11,6 +12,17 @@ class Usuarios{
     }
 
     
+  
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($i)
+    {
+        $this->id = $id;
+    }
+
     public function getNome()
     {
         return $this->nome;
@@ -61,4 +73,39 @@ class Usuarios{
         }
          
     }
+
+    public function consultaCadastro($i){
+        $sql = "SELECT id FROM usuarios WHERE id = :id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(':id', $this->id);
+        $stmt->execute();
+
+        if ($stmt->rowCount() > 0) {
+            return $stmt->fetch();
+        }else{
+            return array();
+        }
+    }
+
+    public function salvarCadastro(){
+        if (consultaCadastro() == true) {
+            $sql = "INSERT INTO usuarios (nome, senha, email, tipo) VALUES (:nome, :senha, :email, :tipo)";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->bindValue(':nome', $this->nome);
+            $stmt->bindValue(':senha', $this->senha);
+            $stmt->bindValue(':email', $this->email);
+            $stmt->bindValue(':tipo', $this->tipo);
+            $stmt->execute();
+        }else {
+            $sql = "UPDATE usuarios SET nome = :nome, senha = :senha, email = :email, tipo = :tipo";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->bindValue(':nome', $this->nome);
+            $stmt->bindValue(':senha', $this->senha);
+            $stmt->bindValue(':email', $this->email);
+            $stmt->bindValue(':tipo', $this->tipo);
+            $stmt->execute();
+        }
+    }
+
+
 }
