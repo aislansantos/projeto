70aa16f9a8cd85800e189b4af3f1a1307c7c04fb
Aislan Aprovar <aislan_85@yahoo.com.br>
2020-08-31 13:47:31 -0300

    comentario config

--- config.php
+++ config.php
@@ -1,12 +1,23 @@
 <?php
-$dsn="mysql:dbname=projeto;host=localhost";
+
+/** Iniciamos variáveis para o PDO, no caso de manutenção podemos
+ * alterar as variáveis ficando o codigo sem alteração
+ */
+/** variavel do nome do banco e onde esta o servidor */
+$dsn = "mysql:dbname=projeto;host=localhost";
+/** variável com usuário que acessa o banco */
 $dbuser = "root";
-$dbpass ="";
+/** variável com senha que acessa o banco */
+$dbpass = "";
 
-
+/** requisitamos a tentativa de acesso a base de dados */
 try {
-$pdo = new PDO($dsn,$dbuser,$dbpass);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    /** Instanciamos o PDO com os dados das variaveis */
+    $pdo = new PDO($dsn, $dbuser, $dbpass);
+    /** Confirmasr depois o que faz essa linha */
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    /** No case de erro pegamos o erro do pdo */
 } catch (PDOException $e) {
-    echo "Error ".$e->getMessage();
+    /** imprimi se houver algum erro, o proprio */
+    echo "Error " . $e->getMessage();
 }
