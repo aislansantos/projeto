41269b8908690d2abaae4b15507898a71acb176b
Aislan Aprovar <aislan_85@yahoo.com.br>
2020-09-02 16:15:35 -0300

    teste

--- footer.php
+++ footer.php
@@ -1,2 +1,4 @@
+<!-- fecha o body de todos os html -->
 </body>
+<!-- fecha o html de todas as paginas -->
 </html>
--- index.php
+++ index.php
@@ -1,15 +1,22 @@
 <?php
+/** iniciamos a sessão */
 session_start();
 
-
+/** verificamos se o usuario da session está vazio ou o status
+ * não está ativo, uma das condições não bater a sessão é terminada e
+ * voltamos para a tela de login
+ */
 if (empty($_SESSION['user']) && $_SESSION['ativo'] != true) {
     session_destroy();
     header("Location: login.php");
 } else {
+    /** se os dados para a session estiver certo ele vai pra tela principal
+     * do sistema.
+     */
     header("Location: principal.php");
 }
 
-if ($_GET[acao] === 'sair') {
+if ($_GET['acao'] === 'sair') {
     session_destroy();
     header("Location: login.php");
 }
